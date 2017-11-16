db856feef895838e95bd2cc5e69904a979046c56
michael <mike.partin@gmail.com>
2017-11-16 11:09:55 -0500

    tokenizing working

--- extraction.py
+++ extraction.py
@@ -1,54 +1,55 @@
 # This code uses the crome_ontology to f
 import rdflib
 from rdflib import Graph
+import sys
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+reload(sys)
+sys.setdefaultencoding('utf8')
 
+st = StanfordNERTagger('stanford-ner-2017-06-09/classifiers/english.muc.7class.distsim.crf.ser.gz',
+                                           'stanford-ner-2017-06-09/stanford-ner.jar',
+                                           encoding='utf-8')
 
-example_text = "police found PERSON"
+try:
+    input_filename = sys.argv[1]
+except:
+    print("ERROR: Missing input file.")
+    sys.exit(1)
+
+with open(input_filename, 'r') as myfile:
+    loaded_text=myfile.read().replace('\n', '')
+
+tokenized_text = word_tokenize(loaded_text)
+classified_text = st.tag(tokenized_text)
+
+tokenized_display = ""
+
+for token in classified_text:
+    if token[1] == "O":
+        tokenized_display = tokenized_display + str(token[0]) + " "
+    else:
+        tokenized_display = tokenized_display + str(token[1]) + " "
+
+print("")
+print("gun violence extractor v0.001")
+print("")
+print("")
+print("------------------------------------")
+print("loaded text:")
+print(loaded_text)
+print("")
+print("tokenized text:")
+print(tokenized_display)
+print("------------------------------------")
+
+text_array = loaded_text.split()
 
 g = Graph()
 g.parse('crime_news.owl')
 
-query_basic = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?s ?p ?o .
-}
-"""
 
-query_authorities = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?s dbo:apprehends ?o .
-    ?s w3:label ?authorities .
-    #?o w3:label ?victim
-}
-"""
-
-query_victims = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?s dbo:apprehends ?o .
-    #?s w3:label ?authorities .
-    ?o w3:label ?victim
-}
-"""
-
-query_ask = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?s w3:label "dude" .
-    ?o w3:label "PERSON" .
-    ?s dbo:apprehends ?o
-}
-"""
-
-
-
-query_ask_2 = """
+test_query = """
 PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
 PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT * WHERE {
@@ -59,61 +60,26 @@ SELECT * WHERE {
 }
 """
 
-
-auth=[]
-a = g.query(query_authorities)
-for row in a:
-    #print(row)
-    for item in row:
-        if isinstance(item, rdflib.term.Literal):
-            auth.append(item)
-
-victims=[]
-a = g.query(query_victims)
-for row in a:
-    #print(row)
-    for item in row:
-        if isinstance(item, rdflib.term.Literal):
-            victims.append(item)
-"""
-a = g.query(query_ask)
-if a:
-  print("Yup")
-else:
-  print("Nope")
-for row in a:
-    if row:
-      print("Yup")
-    #print(row)
-    #for item in row:
-    #    if isinstance(item, rdflib.term.Literal):
-    #        victims.append(item)
+is_victim_query = """
+PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT * WHERE {
+    ?s w3:label "PERSON" .
+    ?p w3:domain ?s .
+    ?p w3:range ?o .
+    ?o w3:label ?label
+}
 """
 
-
-print("query test")
-a = g.query(query_ask_2)
+print("")
+print("------------------------------------")
+print("query test:")
+a = g.query(test_query)
 if a:
-  #print(auth)
-  #print(victims)
   for d in a:
     print(d)
   print("Yup")
 else:
   print("nope")
-#for row in a:
-#    for item in row:
-#        print(item)
-    #print(row)
-    #for item in row:
-    #    if isinstance(item, rdflib.term.Literal):
-    #        victims.append(item)
+print("------------------------------------")
 
-"""
-print("complete db")
-a = g.query(query_basic)
-for row in a:
-    print (row)
-
-
-"""
