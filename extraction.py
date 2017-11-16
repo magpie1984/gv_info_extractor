978cf95d484f3b84cc5f3e19a509cbe46ace2627
michael <mike.partin@gmail.com>
2017-11-16 16:57:00 -0500

    working on ontology query

--- extraction.py
+++ extraction.py
@@ -68,22 +68,29 @@ is_victim_query = """
 PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
 PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT * WHERE {
-    ?s w3:label "PERSON" .
-    ?p w3:domain ?s .
-    ?p w3:range ?o .
-    ?o w3:label ?label
+    dbo:Assailant w3:label ?assailant_label .
+    dbo:Harms w3:domain dbo:Assailant .
+    dbo:Harms w3:range dbo:Victim .
+    dbo:Harms w3:label ?harms_label .
+    dbo:Victim w3:label ?victim_label
 }
 """
 
 print("")
 print("------------------------------------")
 print("query test:")
-a = g.query(test_query)
+a = g.query(is_victim_query)
 if a:
   for d in a:
-    print(d)
-  print("Yup")
+    for m in d:
+        if "#" in m:
+            print(m.split("#")[1])
+        else:
+            print(m)
+        #print("\n")
+  #print("Yup")
 else:
-  print("nope")
+  pass
+  #print("nope")
 print("------------------------------------")
 
