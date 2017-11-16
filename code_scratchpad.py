28cc618c8af2aef6191e610e3f33bee503119d0b
michael <mike.partin@gmail.com>
2017-11-16 11:09:29 -0500

    code snippet scratch pad

---
+++ code_scratchpad.py
@@ -0,0 +1,89 @@
+query_basic = """
+PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT * WHERE {
+    ?s ?p ?o .
+}
+"""
+
+query_authorities = """
+PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT * WHERE {
+    ?s dbo:apprehends ?o .
+    ?s w3:label ?authorities .
+    #?o w3:label ?victim
+}
+"""
+
+query_victims = """
+PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT * WHERE {
+    ?s dbo:apprehends ?o .
+    #?s w3:label ?authorities .
+    ?o w3:label ?victim
+}
+"""
+
+
+query_ask = """
+PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT * WHERE {
+    ?s w3:label "dude" .
+    ?o w3:label "PERSON" .
+    ?s dbo:apprehends ?o
+}
+"""
+
+  #print(auth)
+  #print(victims)
+
+#for row in a:
+#    for item in row:
+#        print(item)
+    #print(row)
+    #for item in row:
+    #    if isinstance(item, rdflib.term.Literal):
+    #        victims.append(item)
+
+"""
+print("complete db")
+a = g.query(query_basic)
+for row in a:
+    print (row)
+
+
+"""
+
+
+"""
+a = g.query(query_ask)
+if a:
+  print("Yup")
+else:
+  print("Nope")
+for row in a:
+    if row:
+      print("Yup")
+    #print(row)
+    #for item in row:
+    #    if isinstance(item, rdflib.term.Literal):
+    #        victims.append(item)
+"""
+
+
+auth=[]
+a = g.query(query_authorities)
+for row in a:
+    for item in row:
+        if isinstance(item, rdflib.term.Literal):
+            auth.append(item)
+
+victims=[]
+a = g.query(query_victims)
+for row in a:
+    for item in row:
+        if isinstance(item, rdflib.term.Literal):
+            victims.append(item)
