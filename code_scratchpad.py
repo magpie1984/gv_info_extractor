4e37210ca18f3e1650f440dd7e25eb4019fac2c9
michael <mike.partin@gmail.com>
2017-11-17 17:47:01 -0500

    changed script into class form and created a ontology query function

--- code_scratchpad.py
+++ code_scratchpad.py
@@ -37,6 +37,44 @@ SELECT * WHERE {
 }
 """
 
+
+
+query = """
+        PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+        PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT * WHERE {
+            ?s w3:label """ + "\"" + str(subject_label) + "\"" + """ .
+            ?p w3:label """ + "\"" + str(predicate_label) + "\"" + """ .
+            ?p w3:domain dbo:Assailant .
+            ?p w3:range dbo:Victim .
+            ?o w3:label """ + "\"" + str(object_label) + "\"" + """
+        }
+        """
+
+
+
+        test_query = """
+        PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+        PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT * WHERE {
+            ?s w3:label "PERSON" .
+            ?p w3:domain ?s .
+            ?p w3:range ?o .
+            ?o w3:label ?label
+        }
+        """
+
+        is_victim_query = """
+        PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+        PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT * WHERE {
+            ?s w3:label """ + str("\"PERSON\"") + """ .
+            ?p w3:label """ + str("\"shoots\"") + """ .
+            ?p w3:domain dbo:Assailant .
+            ?p w3:range dbo:Victim .
+            ?o w3:label """ + str("\"PERSON\"") + """
+        }
+        """
   #print(auth)
   #print(victims)
 
@@ -87,3 +125,28 @@ for row in a:
     for item in row:
         if isinstance(item, rdflib.term.Literal):
             victims.append(item)
+
+
+
+        """
+        print("")
+        print("------------------------------------")
+        print("query test:")
+        a = self.g.query(is_victim_query)
+        if a:
+          for d in a:
+            print (d)
+            #for m in d:
+            #    if "#" in m:
+            #        print(m)
+            #        #print(m.split("#")[1])
+            #    else:
+            #        print(m)
+            #    #print("\n")
+          #print("Yup")
+        else:
+          pass
+          #print("nope")
+        print("------------------------------------")
+
+       """ 
--- extraction.py
+++ extraction.py
@@ -7,90 +7,88 @@ from nltk.tokenize import word_tokenize
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-st = StanfordNERTagger('stanford-ner-2017-06-09/classifiers/english.muc.7class.distsim.crf.ser.gz',
-                                           'stanford-ner-2017-06-09/stanford-ner.jar',
-                                           encoding='utf-8')
+class Extractor:
 
-try:
-    input_filename = sys.argv[1]
-except:
-    print("ERROR: Missing input file.")
-    sys.exit(1)
+    def __init__(self):
 
-with open(input_filename, 'r') as myfile:
-    loaded_text=myfile.read().replace('\n', '')
+        stan_7_class = 'stanford-ner-2017-06-09/classifiers/english.muc.7class.distsim.crf.ser.gz'
+        stan_ner = 'stanford-ner-2017-06-09/stanford-ner.jar'
+        st = StanfordNERTagger(stan_7_class, stan_ner, encoding='utf-8')
 
-tokenized_text = word_tokenize(loaded_text)
-classified_text = st.tag(tokenized_text)
-
-tokenized_display = ""
-
-for token in classified_text:
-    if token[1] == "O":
-        tokenized_display = tokenized_display + str(token[0]) + " "
-    else:
-        tokenized_display = tokenized_display + str(token[1]) + " "
-
-print("")
-print("gun violence extractor v0.001")
-print("")
-print("")
-print("------------------------------------")
-print("loaded text:")
-print(loaded_text)
-print("")
-print("tokenized text:")
-print(tokenized_display)
-print("------------------------------------")
+        try:
+            input_filename = sys.argv[1]
+        except:
+            print("ERROR: Missing input file.")
+            sys.exit(1)
 
 
-print("")
-print("comparison:")
-for t, c in zip(tokenized_text, classified_text):
-    print(t.encode('ascii','ignore'), c[1].encode('ascii','ignore'))
+        with open(input_filename, 'r') as myfile:
+            loaded_text=myfile.read().replace('\n', '')
 
-g = Graph()
-g.parse('crime_news.owl')
+        tokenized_text = word_tokenize(loaded_text)
+        classified_text = st.tag(tokenized_text)
+
+        tokenized_display = ""
+
+        for token in classified_text:
+            if token[1] == "O":
+                tokenized_display = tokenized_display + str(token[0]) + " "
+            else:
+                tokenized_display = tokenized_display + str(token[1]) + " "
+
+        print("")
+        print("gun violence extractor v0.001")
+        print("")
+        print("")
+        print("------------------------------------")
+        print("loaded text:")
+        print(loaded_text)
+        print("")
+        print("tokenized text:")
+        print(tokenized_display)
+        print("------------------------------------")
 
 
-test_query = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    ?s w3:label "PERSON" .
-    ?p w3:domain ?s .
-    ?p w3:range ?o .
-    ?o w3:label ?label
-}
-"""
+        print("")
+        print("comparison:")
+        #for t, c in zip(tokenized_text, classified_text):
+        #    print(t.encode('ascii','ignore'), c[1].encode('ascii','ignore'))
 
-is_victim_query = """
-PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
-PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT * WHERE {
-    dbo:Assailant w3:label ?assailant_label .
-    dbo:Harms w3:domain dbo:Assailant .
-    dbo:Harms w3:range dbo:Victim .
-    dbo:Harms w3:label ?harms_label .
-    dbo:Victim w3:label ?victim_label
-}
-"""
+        self.g = Graph()
+        self.g.parse('crime_news.owl')
 
-print("")
-print("------------------------------------")
-print("query test:")
-a = g.query(is_victim_query)
-if a:
-  for d in a:
-    for m in d:
-        if "#" in m:
-            print(m.split("#")[1])
-        else:
-            print(m)
-        #print("\n")
-  #print("Yup")
-else:
-  pass
-  #print("nope")
-print("------------------------------------")
 
+    def query_ontology(self, subject_label, predicate_label, object_label):
+        query = """
+        PREFIX dbo: <http://www.semanticweb.org/michael/ontologies/crime_ontology#>
+        PREFIX w3: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT * WHERE {
+            ?s w3:label """ + "\"" + str(subject_label) + "\"" + """ .
+            ?p w3:label """ + "\"" + str(predicate_label) + "\"" + """ .
+            ?p w3:domain ?s .
+            ?p w3:range ?o .
+            ?o w3:label """ + "\"" + str(object_label) + "\"" + """
+        }
+        """
+        a = self.g.query(query)
+
+        relationship = ""
+
+        if a:
+            for d in a:
+                for m in d:
+                    if "#" in m:
+                        if len(relationship) == 0:
+                            relationship = relationship + m.split("#")[1]
+                        else:
+                            relationship = relationship + " " + m.split("#")[1]
+                    else:
+                        if len(relationship) == 0:
+                            relationship = relationship + m
+                        else:
+                            relationship = relationship + " " + m
+
+        return relationship
+
+a = Extractor()
+print (a.query_ontology("PERSON", "shoots", "PERSON"))
